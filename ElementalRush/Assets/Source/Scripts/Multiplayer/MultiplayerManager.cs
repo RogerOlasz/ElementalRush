47fbe75cb49a5c8874fcbde0777cd3c3af7ce8e9
Roger Olasz <rogerolasz96@gmail.com>
2019-05-04 16:29:02 +0200

    Adding multiplayer system. Necessary libraries and unity packages included. Created the Playfab Manager and Multiplayer Manager and setted up the player as Prefab. Still not working, setting up the connection with the server.

---
+++ ElementalRush/Assets/Source/Scripts/Multiplayer/MultiplayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MultiplayerManager : MonoBehaviourPun
+{
+    public PlayfabManager playfab_manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ConnectToMaster()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+    }
+
+    public void CreateOrJoin()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnPhotonRandomJoinFailed()
+    {
+        RoomOptions new_room = new RoomOptions();
+        new_room.MaxPlayers = 6;
+        new_room.IsVisible = true;
+
+        int random_id = Random.Range(0, 3000);
+
+        PhotonNetwork.CreateRoom("Dev: " + random_id, new_room, TypedLobby.Default);
+    }
+
+    public virtual void OnJoinedRoom()
+    {  
+        GameObject player = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+    }
+}
---
+++ ElementalRush/Assets/Source/Scripts/Multiplayer/PlayfabManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabManager : MonoBehaviour
+{
+    public MultiplayerManager multiplayer_manager;
+
+    public string username;
+    public string password;
+    public bool have_account = false;
+
+    LoginWithPlayFabRequest login_request;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(have_account == false)
+        {
+            Register();
+            Login();
+        }
+        else if(have_account == true)
+        {
+            Login();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Login()
+    {
+        login_request = new LoginWithPlayFabRequest();
+        login_request.Username = username;
+        login_request.Password = password;
+
+        PlayFabClientAPI.LoginWithPlayFab(login_request,
+        result =>
+        {
+            //If the account is found
+            multiplayer_manager.ConnectToMaster();
+            Debug.Log("You are now logged in!");
+        }, error =>
+        {
+            // If the account is not found
+            Debug.Log(error.ErrorMessage);
+        }, null);
+    }
+
+    public void Register()
+    {
+        RegisterPlayFabUserRequest request = new RegisterPlayFabUserRequest();
+        request.Username = username;
+        request.Password = password;
+
+        PlayFabClientAPI.RegisterPlayFabUser(request,
+        result =>
+        {
+            Debug.Log("Your account has been created succesfully!");
+        }, error =>
+        {
+            Debug.Log("Failed to create your account\n [" + error.ErrorMessage + "]");
+        });
+    }
+}
