550fce7f91c108a44d8f67f1f5b88df084aa4615
Roger Olasz <rogerolasz96@gmail.com>
2019-05-18 17:55:07 +0200

    Finally it worked, now the UI player panels positions are sincronized well enought. Still a lot to polish but it is at leas an acceptable version. Lets move on to another system.

--- ElementalRush/Assets/Source/Scripts/PlayerPanel.cs
+++ ElementalRush/Assets/Source/Scripts/PlayerPanel.cs
@@ -23,12 +23,7 @@ public class PlayerPanel : MonoBehaviourPun, IPunObservable
         {
             my_rect = GetComponent<RectTransform>();
             player_camera = Camera.main;
-            canvas = transform.parent.GetComponent<Canvas>();
-            
-
-            //my_rect.anchoredPosition = new Vector3(0, 25, 0);
-            //Debug.Log("Screen pos: " + Camera.main.WorldToScreenPoint(my_player.transform.position));
-            //Debug.Log("World pos: " + my_player.transform.position);
+            canvas = transform.parent.GetComponent<Canvas>();           
         }        
     }
 
@@ -49,7 +44,6 @@ public class PlayerPanel : MonoBehaviourPun, IPunObservable
     {
         if (photonView.IsMine)
         {
-            Debug.Log("Is Mine: " + photonView.ViewID);
             Vector2 position_on_screen = player_camera.WorldToScreenPoint(PhotonView.Find((photonView.ViewID - 1)).gameObject.transform.position);
 
             Vector2 final_position = new Vector2((position_on_screen.x / canvas.scaleFactor) + x_offset, (position_on_screen.y / canvas.scaleFactor) + y_offset);
@@ -57,43 +51,21 @@ public class PlayerPanel : MonoBehaviourPun, IPunObservable
         }
         else
         {
-            Debug.Log("Else: " + photonView.ViewID);
-            //Debug.Log(PhotonView.Find((photonView.ViewID - 1)).gameObject.transform.position);
-            Vector3 player_pos = PhotonView.Find((photonView.ViewID - 1)).gameObject.transform.position;
-            Vector2 position_on_screen = player_camera.WorldToScreenPoint(player_pos);
-            //Debug.Log(position_on_screen);
+            //Vector3 player_pos = PhotonView.Find((photonView.ViewID - 1)).gameObject.transform.position;
+            //Vector2 position_on_screen = Camera.main.WorldToScreenPoint(player_pos);
 
-            Vector2 final_position = new Vector2((position_on_screen.x / canvas.scaleFactor) + x_offset, (position_on_screen.y / canvas.scaleFactor) + y_offset);
-            my_rect.anchoredPosition = final_position;
+            //Vector2 final_position = new Vector2((position_on_screen.x / transform.parent.GetComponent<Canvas>().scaleFactor) + x_offset, (position_on_screen.y / transform.parent.GetComponent<Canvas>().scaleFactor) + y_offset);
+            //this.GetComponent<RectTransform>().anchoredPosition = final_position;
+            //Debug.Log(Camera.main.transform.position);
+
+            Vector2 position_on_screen = Camera.main.WorldToViewportPoint(PhotonView.Find((photonView.ViewID - 1)).gameObject.transform.position);
+
+            Debug.Log(position_on_screen);
+            position_on_screen.y += 0.075f;
+
+            this.GetComponent<RectTransform>().anchorMin = position_on_screen;
+            this.GetComponent<RectTransform>().anchorMax = position_on_screen;
         }
-        //Debug.Log("Player Panel ID: " + photonView.ViewID + "\nPlayer position:" + my_player.transform.position);
-        //photonView.RPC("Test", RpcTarget.All, photonView.ViewID);
-    }
-
-    //[PunRPC]
-    //void Test(PhotonMessageInfo info)
-    //{
-    //    //Vector2 position_on_screen = player_camera.WorldToScreenPoint(my_player.transform.position);
-    //    my_player = (GameObject)info.photonView.Owner.TagObject;
-        
-    //    Vector2 position_on_screen = player_camera.WorldToScreenPoint(my_player.transform.position);
-
-    //    Vector2 final_position = new Vector2((position_on_screen.x / canvas.scaleFactor) + x_offset, (position_on_screen.y / canvas.scaleFactor) + y_offset);
-    //    my_rect.anchoredPosition = final_position;
-    //}
-
-    [PunRPC]
-    void Test(int sender_view, PhotonMessageInfo info)
-    {
-        //Debug.Log("Sender: " + info.Sender + "\nPhotonView: " + info.photonView);
-        Debug.Log(sender_view);
-
-        //my_player = PhotonView.Find(sender_view).gameObject;
-        //
-        //Vector2 position_on_screen = player_camera.WorldToScreenPoint(my_player.transform.position);
-        //
-        //Vector2 final_position = new Vector2((position_on_screen.x / canvas.scaleFactor) + x_offset, (position_on_screen.y / canvas.scaleFactor) + y_offset);
-        //my_rect.anchoredPosition = final_position;
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
