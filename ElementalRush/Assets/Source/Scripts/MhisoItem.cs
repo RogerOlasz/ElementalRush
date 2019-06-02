a3e8b61d84053ad1a56bb24b38e13b64dfb55146
Roger Olasz <rogerolasz96@gmail.com>
2019-06-02 13:24:48 +0200

    Item system added. Players now can take 4 different types of resources and carry them, if someone from the other team touches them, they lose the item.

---
+++ ElementalRush/Assets/Source/Scripts/MhisoItem.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MhisoItem : MonoBehaviourPun
+{
+    PlayerItemManager player_item_manager_script;
+    [SerializeField] private PlayerItemManager.Items my_type;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            player_item_manager_script = collider.GetComponent<PlayerItemManager>();
+            player_item_manager_script.SetItemCarrying(my_type);
+
+            photonView.RPC("ItemTaken", RpcTarget.All);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+    [PunRPC]
+    public void ItemTaken()
+    {
+        StartCoroutine("MhisoRespawn");
+    }
+
+    IEnumerator MhisoRespawn()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+
+        yield return new WaitForSeconds(3);
+
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<BoxCollider>().enabled = true;
+
+        StopCoroutine(MhisoRespawn());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.name == "Fountain_Mhiso_Orange")
+        {
+            my_type = PlayerItemManager.Items.Tier1_Orange;
+        }
+        else if (gameObject.name == "Fountain_Mhiso_Lime")
+        {
+            my_type = PlayerItemManager.Items.Tier1_Lime;
+        }
+        else if (gameObject.name == "Fountain_Mhiso_Fuchsia")
+        {
+            my_type = PlayerItemManager.Items.Tier1_Fuchsia;
+        }
+        else if (gameObject.name == "Fountain_Mhiso_Turquoise")
+        {
+            my_type = PlayerItemManager.Items.Tier1_Turquoise;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ElementalRush/Assets/Source/Scripts/PlayerItemManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemManager : MonoBehaviour
+{
+    public enum Items
+    {
+        None = 0,
+        Tier1_Orange,
+        Tier1_Lime,
+        Tier1_Turquoise,
+        Tier1_Fuchsia
+    };
+
+    Items item_carrying;
+
+    public void SetItemCarrying(Items item_to_carry)
+    {
+        item_carrying = item_to_carry;
+    }
+
+    public void DropItem()
+    {
+        item_carrying = Items.None;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.layer != this.gameObject.layer)
+            {
+                this.DropItem();
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        item_carrying = Items.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
